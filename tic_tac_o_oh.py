c554907ccaaf37d45972bf883bb1a46a227ce89e
20148528 <20148528@tafe.wa.edu.au>
2026-06-01 18:48:48 +0800

    refactor: updated to execute the game

--- tic_tac_o_oh.py
+++ tic_tac_o_oh.py
@@ -1,54 +1,56 @@
-'''A monolithic and poorly written tic-tac-toe for you to refactor.'''
 from src.board import Board
 from src.game import Game
 
 
 # Game state
-p1 = "X"
-p2 = "O"
+def run_game_main():
+    p1 = "X"
+    p2 = "O"
+    board = Board()
+    game = Game()
 
-board = Board()
-game = Game()
-
-# Game loop
-while True:
-    # Print board
-    board.display()
-
-    # Check for win - Game.py
-    winner = game.check_for_win(board.board)
-    if winner is not None:
-        print("Player", winner, "win!")
-        break
-
-    # Check for tie - game.py
-    if game.check_for_tie(board.board):
-        print("Tie!")
-        break
-
-
-    # Get next move
+    # Game loop
     while True:
-        empty_count = 0
-        for row in board.board:
-            for cell in row:
-                if cell == " ":
-                    empty_count += 1
+        # Print board
+        board.display()
 
-        player = p1 if empty_count % 2 == 0 else p2
-        move = input("Enter your move: " + player + "(0-8): ")
+        # Check for win - Game.py
+        winner = game.check_for_win(board.board)
+        if winner is not None:
+            print("Player", winner, "win!")
+            break
 
-        if move.isdigit() and 0 <= int(move) <= 8:
-            row = int(move) // 3
-            col = int(move) % 3
+        # Check for tie - game.py
+        if game.check_for_tie(board.board):
+            print("Tie!")
+            break
 
-            if board.is_empty(row, col):
-                board.update(row, col, player)
-                break
+
+        # Get next move
+        while True:
+            empty_count = 0
+            for row in board.board:
+                for cell in row:
+                    if cell == " ":
+                        empty_count += 1
+
+            player = p1 if empty_count % 2 == 0 else p2
+            move = input("Enter your move: " + player + "(0-8): ")
+
+            if move.isdigit() and 0 <= int(move) <= 8:
+                row = int(move) // 3
+                col = int(move) % 3
+
+                if board.is_empty(row, col):
+                    board.update(row, col, player)
+                    break
+                else:
+                    print("Invalid move!")
+
             else:
                 print("Invalid move!")
 
-        else:
-            print("Invalid move!")
 
+if __name__ == "__main__":
+    run_game_main()
 
