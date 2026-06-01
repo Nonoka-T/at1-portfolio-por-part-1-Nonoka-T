aa65cb23c94739fb7a502f6c7b2edbc17d0f94f2
20148528 <20148528@tafe.wa.edu.au>
2026-06-01 20:05:09 +0800

    test: added unittest

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -2,16 +2,48 @@ import unittest
 from src.board import Board
 from src.game import Game
 
-class TestTicTacToe(unittest.TestCase):
-    def test_check_win(self):
-        game = Game()
 
+class TestTicTacToe(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Game()
+
+    def test_row_win(self):
+        """Test horizontal win is detected or not"""
         board = [
             ["X", "X", "X"],
             [" ", " ", " "],
             [" ", " ", " "]
         ]
-        self.assertEqual(game.check_for_win(board), "X")
+        self.assertEqual(self.game.check_for_win(board), "X")
+
+    def test_column_win(self):
+        """Test vertical win is detected or not"""
+        board = [
+            ["O", " ", " "],
+            ["O", " ", " "],
+            ["O", " ", " "]
+        ]
+        self.assertEqual(self.game.check_for_win(board), "O")
+
+    def test_diagonal_win_top_right(self):
+        """Test top right diagonal win"""
+        board = [
+            [" ", " ", "O"],
+            [" ", "O", " "],
+            ["O", " ", " "]
+        ]
+        self.assertEqual(self.game.check_for_win(board), "O")
+
+    def test_diagonal_win_top_left(self):
+        """Test top left diagonal win"""
+        board = [
+            ["X", " ", " "],
+            [" ", "X", " "],
+            [" ", " ", "X"]
+        ]
+        self.assertEqual(self.game.check_for_win(board), "X")
+
 
 if __name__ == '__main__':
     unittest.main()
